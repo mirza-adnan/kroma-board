dd5249f9a175769268ba12d96781bd6cd6fac5ab
Mirza Adnan <mirza.adnan2205@gmail.com>
2024-07-17 14:54:13 +0600

    edit better regex for color matching

--- client/src/lib/colorPatterns.js
+++ client/src/lib/colorPatterns.js
@@ -1,14 +1,30 @@
-export const hexPattern = /(#?[0-9A-Fa-f]{3,6})/;
-export const hslPattern =
-  /^(hsl\(\s*(\d{1,3})\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*\))$/;
-export const rgbPattern =
-  /^(rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\))$/;
-export const hslaPattern =
-  /^(hsla\(\s*(\d{1,3})\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*,\s*(0(\.\d+)?|1(\.0+)?)\))$/;
-export const rgbaPattern =
-  /^(rgba\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(0(\.\d+)?|1(\.0+)?)\))$/;
-export const universalPattern =
-  /^(hsla\(\s*(\d{1,3})\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*,\s*(0(\.\d+)?|1(\.0+)?)\))|(hsl\(\s*(\d{1,3})\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*\))|(#?[0-9A-Fa-f]{3,6})|(rgba\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(0(\.\d+)?|1(\.0+)?)\))|(rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\))$/;
+export const hexPattern = new RegExp(/^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/);
+export const hslPattern = new RegExp(
+  /^(hsl\(\s*(\d{1,3})\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*\))$/
+);
+export const rgbPattern = new RegExp(
+  /^(rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\))$/
+);
+export const hslaPattern = new RegExp(
+  /^(hsla\(\s*(\d{1,3})\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*,\s*(0(\.\d+)?|1(\.0+)?)\))$/
+);
+export const rgbaPattern = new RegExp(
+  /^(rgba\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(0(\.\d+)?|1(\.0+)?)\))$/
+);
+// export const universalPattern =
+//   /^(hsla\(\s*(\d{1,3})\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*,\s*(0(\.\d+)?|1(\.0+)?)\))|(hsl\(\s*(\d{1,3})\s*,\s*(\d{1,3})%\s*,\s*(\d{1,3})%\s*\))|(#?[0-9A-Fa-f]{3,6})|(rgba\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(0(\.\d+)?|1(\.0+)?)\))|(rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\))$/;
+
+export const universalPattern = new RegExp(
+  hexPattern.source +
+    "|" +
+    hslPattern.source +
+    "|" +
+    rgbPattern.source +
+    "|" +
+    hslaPattern.source +
+    "|" +
+    rgbaPattern.source
+);
 
 export const validateColor = (val) => {
   console.log(val);
